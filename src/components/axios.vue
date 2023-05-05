3737010dd354b664cdcd7623a595308683202869
PLENGOP03 <128958362+PLENGOP03@users.noreply.github.com>
2023-05-05 14:06:15 +0700

    Initial commit

---
+++ src/components/axios.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p v-for="menu in menufactuer" : key="menu.id">
+      {{ menu.id }} - {{ menu.Title }}
+    </p>
+    <input placeholder="id" v-model="id" />
+    <input placeholder="Title" v-model="Title" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      text: "test",
+      menufactuer: [{ id: 1, Title: "test1" }],
+    };
+  },
+  created() {
+    this.get();
+  },
+  methods: {
+    get() {
+      axios.get("https://localhost:7148/api/menufacturers").then((Response) => {
+        this.menufacturer = respone.data;
+      });
+    },
+  },
+};
+</script>>
